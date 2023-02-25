4ed3f0c9030518d4cc00ddefea43bd8dcfc27ea8
Thiago Luiz <tluiz199@gmail.com>
2023-02-25 19:07:28 -0300

    adicionado nova frase no regulamento

--- client/src/pages/Regulamento.tsx
+++ client/src/pages/Regulamento.tsx
@@ -4,7 +4,12 @@ import { Container } from "react-bootstrap";
 export function Regulamento() {
   return (
     <Container className="bor-regulamento">
-      <span>Seja Feliz e não perca para o borraxa</span>
+      <span>
+        Pode jogar livremente as partidas da ida, envie o resultado no discord
+        "CDRZÃO".
+      </span>
+      <p></p>
+      <span>Seja Feliz e não perca para o borraxa!</span>
     </Container>
   );
 }
