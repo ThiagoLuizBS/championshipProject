0def9f7c8a9705f8af22609fb5a51a61f102a6f6
Thiago Luiz <tluiz199@gmail.com>
2023-04-25 20:25:20 -0300

    database adicionado ao main

--- client/src/services/axios.tsx
+++ client/src/services/axios.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
-  //   baseURL: "https://championship-project-9hyg.vercel.app/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://championship-project-9hyg.vercel.app/api",
   headers: {
     "Content-type": "application/json",
   },
